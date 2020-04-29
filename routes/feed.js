4a301875375ea6058ef9c3f7ec8872ddb7db5015
Lauri Kyttälä <lauri.kyttala@gmail.com>
2020-04-29 12:52:47 +0300

    added server side validation

--- routes/feed.js
+++ routes/feed.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const { body } = require('express-validator/check')
 
 const feedController = require('../controllers/feed')
 
@@ -7,6 +8,13 @@ const router = express.Router()
 // GET /feed/posts
 router.get('/posts', feedController.getPosts)
 // POST /feed/post
-router.post('/post', feedController.createPost)
+router.post('/post', [
+  body('title')
+    .trim()
+    .isLength({ min: 5 }),
+  body('content')
+    .trim()
+    .isLength({ min: 5 }),
+], feedController.createPost)
 
 module.exports = router
