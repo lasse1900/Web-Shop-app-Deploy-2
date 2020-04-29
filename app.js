ceb03882706bc05906c2821ba0e53d55c9d49445
Lauri Kyttälä <lauri.kyttala@gmail.com>
2020-04-29 17:00:33 +0300

    uploading images

--- app.js
+++ app.js
@@ -5,15 +5,56 @@ const path = require('path')
 const express = require('express')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
+const multer = require('multer')
 
 const feedRoutes = require('./routes/feed')
 
 const app = express()
 
+// const fileStorage = multer.diskStorage({
+//   destination: (req, file, cb) => {
+//     cb(null, 'images')
+//   },
+//   filename: (req, file, cb) => {
+//     cb(null, new Date().toISOString() + '-' + file.originalname)
+//   }
+// })
+
+// const fileFilter = (req, file, cb) => {
+//   if (file.mimetype === 'image/png' || file.mimetype === 'image.jpeg' || file.mimetype === 'image.jpg') {
+//     cb(null, true)
+//   } else {
+//     cb(null, false)
+//   }
+// }
+const fileStorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'images');
+  },
+  filename: (req, file, cb) => {
+    cb(null, new Date().toISOString() + '-' + file.originalname);
+  }
+});
+
+const fileFilter = (req, file, cb) => {
+  if (
+    file.mimetype === 'image/png' ||
+    file.mimetype === 'image/jpg' ||
+    file.mimetype === 'image/jpeg'
+  ) {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
+
 const MONGODB_URI = process.env.MONGODB_URI
 
 // app.use(bodyParser.urlencoded())  // x-www-form-urlencoded <form>
 app.use(bodyParser.json())  // application/json
+app.use(
+  multer({ storage: fileStorage, fileFilter: fileFilter }).single('image')
+)
 app.use('/images', express.static(path.join(__dirname, 'images')))
 
 app.use((req, res, next) => {
@@ -29,7 +70,7 @@ app.use((error, req, res, next) => {
   console.log(error)
   const status = error.statusCode || 500
   const message = error.message
-  res.status(status).json({message: message})
+  res.status(status).json({ message: message })
 })
 
 mongoose.connect(MONGODB_URI,
