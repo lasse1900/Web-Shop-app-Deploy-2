bd5054b2b780141ffc3f59a1ee4caf04f8d71525
Lauri Kyttälä <lauri.kyttala@gmail.com>
2020-04-29 14:46:14 +0300

    storing Posts into db

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const { validationResult } = require('express-validator')
 
+const Post = require('../models/post')
+
 exports.getPosts = (req, res, next) => {
   res.status(200).json({
     posts: [
@@ -30,15 +32,19 @@ exports.createPost = (req, res, next) => {
   const title = req.body.title
   const content = req.body.content
   console.log(title, content)
-  // create db
-  res.status(201).json({
-    message: 'Post created succefully',
-    post: {
-      _id: new Date().toISOString(),
-      title: title,
-      content: content,
-      creator: { name: 'Lasse' },
-      createdAt: new Date()
-    }
+  const post = new Post({
+    title: title,
+    content: content,
+    imageUrl: 'images/duck.jpg',
+    creator: { name: 'Lasse' },
   })
+  post.save()
+    .then(result => {
+      console.log(result)
+      res.status(201).json({
+        message: 'Post created succefully',
+        post: result
+      })
+    })
+    .catch(err => console.log(err))
 }
