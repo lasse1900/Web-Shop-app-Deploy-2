ac2cbe2c9edca70f326377ea706687b44aaca080
Lauri Kyttälä <lauri.kyttala@gmail.com>
2020-04-29 12:14:21 +0300

    Initial commit

---
+++ controllers/feed.js
@@ -0,0 +1,26 @@
+exports.getPosts = (req, res, next) => {
+  res.status(200).json({
+    posts: [
+      {
+        _id: '23232323',
+        title: 'First post',
+        content: 'This is the first post',
+        imageUrl: 'images/duck.jpg',
+        creator: {
+          name: 'Lasse'
+        },
+        createdAt: new Date()
+      }]
+  })
+}
+
+exports.createPost = (req, res, next) => {
+  const title = req.body.title
+  const content = req.body.content
+  console.log(title, content)
+  // create db
+  res.status(201).json({
+    message: 'Post created succefully',
+    post: { id: new Date().toISOString(), title: title, content: content }
+  })
+}
