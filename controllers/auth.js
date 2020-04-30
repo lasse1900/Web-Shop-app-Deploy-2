d8151a5f9d94d5cac4de156e527735bce493853e
Lauri Kyttälä <lauri.kyttala@gmail.com>
2020-04-30 12:23:32 +0300

    using and validating token

--- controllers/auth.js
+++ controllers/auth.js
@@ -58,7 +58,7 @@ exports.login = (req, res, next) => {
       const token = jwt.sign({
         email: loadedUser.email,
         userId: loadedUser._id.toString()
-      }, 'mysupersecretalgoritm',
+      }, 'mysupersecret',
         { expiresIn: '1h' }
       )
       res.status(200).json({ token: token, userId: loadedUser._id.toString() })
