b139faacb47257f56b1fe78e865e533cda2a1e3f
Lauri Kyttälä <lauri.kyttala@gmail.com>
2020-05-01 16:53:05 +0300

    got rid of user update failure

--- routes/auth.js
+++ routes/auth.js
@@ -3,6 +3,7 @@ const { body } = require('express-validator')
 
 const User = require('../models/user')
 const authController = require('../controllers/auth')
+const isAuth = require('../middleware/is-auth')
 
 const router = express.Router()
 
@@ -28,5 +29,18 @@ router.put(
 ], authController.signup)
 
 router.post('/login', authController.login)
+
+router.get('/status', isAuth, authController.getUserStatus)
+
+router.patch('/status',
+  isAuth,
+  [
+    body('status')
+      .trim()
+      .not()
+      .isEmpty()
+  ],
+  authController.updateUserStatus
+)
 
 module.exports = router
