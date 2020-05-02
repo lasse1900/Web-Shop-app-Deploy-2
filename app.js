7e357a0d7b2ca9c9c358fe74394c297805336169
Lauri Kyttälä <lauri.kyttala@gmail.com>
2020-05-02 12:51:39 +0300

    added socket.io

--- app.js
+++ app.js
@@ -64,6 +64,10 @@ mongoose.connect(MONGODB_URI,
   { useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true })
   .then(result => {
     console.log('listening at port 8080')
-    app.listen(8080)
+    const server = app.listen(8080)
+    const io = require('socket.io')(server)
+    io.on('connection', socket => {
+      console.log('Client connected')
+    })
   }).catch(err => console.log(err))
 
