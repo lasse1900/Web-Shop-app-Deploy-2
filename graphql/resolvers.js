365471f99acd5ecf74fbf6ba78fb3ac27185cce1
Lauri Kyttälä <lauri.kyttala@gmail.com>
2020-05-04 15:26:18 +0300

    added createPost mutation

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,7 +1,9 @@
 const bcrypt = require('bcryptjs')
 const validator = require('validator')
-const User = require('../models/user')
 const jwt = require('jsonwebtoken')
+
+const User = require('../models/user')
+const Post = require('../models/post')
 
 module.exports = {
   createUser: async function ({ userInput }, req) {
@@ -55,5 +57,37 @@ module.exports = {
       { expiresIn: '1h' }
     )
     return { token: token, userId: user._id.toString() }
+  },
+  createPost: async function ({ postInput }, req) {
+    const errors = []
+    if (validator.isEmpty(postInput.title) ||
+      !validator.isLength(postInput.title, { min: 5 })
+    ) {
+      errors.push({ message: 'Title is invalid.' })
+    }
+    if (validator.isEmpty(postInput.content) ||
+      !validator.isLength(postInput.content, { min: 5 })
+    ) {
+      errors.push({ message: 'Content is invalid.' })
+    }
+    if (errors.length > 0) {
+      const error = new Error('Invalid input.')
+      error.data = errors
+      error.code = 422
+      throw error
+    }
+    const post = new Post({
+      title: postInput.title,
+      content: postInput.content,
+      imageUrl: postInput.imageUrl
+    })
+    const createdPost = await post.save()
+    // Add post to users
+    return {
+      ...createPost._doc,
+      _id: createdPost._id.toString(),
+      createdAt: createdPost.createdAt.toString(),
+      updatedAt: createdPost.updatedAt.toString()
+    }
   }
 }
